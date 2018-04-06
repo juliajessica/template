d77652852e1db8d13fcf6ff8963844e0316b8b30
julia sheremet <juliajessicasher@gmail.com>
2018-04-06 16:47:04 -0700

    debugg issues found in console

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,41 @@
 //backend logic
-var pizzaPrice = 5;
-
-function PizzaOrder(size, topping){ //constructor
+function PizzaOrder(price, size, topping){ //constructor
+  this.price = price;
   this.size = size;
   this.topping = topping;
 }
 
 PizzaOrder.prototype.pizzaCalculation = function(){ //create a prototype method to calculate the price
   debugger;
-  if (this.size === "Medium") {
-    pizzaPrice += 2;
+  if (this.size === "Small") {
+    debugger;
+    this.price += 5;
+  } else if (this.size === "Medium") {
+    this.price += 7;
   } else if (this.size === "Large") {
-    pizzaPrice += 3;
+    this.price += 10;
   } else if (this.size === "Extra Large") {
-    pizzaPrice+= 5;
+    this.price += 12;
     console.log(this.price);
   }
-  for (var i=0; i<=i.length;i++) { //looping through array to find which topping items were selected
-    if (this.topping === " Sausage" || this.topping === " Pepperoni") {
-      this.price += 2;
-    } else if (this.topping === " Mushrooms" || this.topping === " Green Peppers" || this.topping === " Olives") {
-      this.price += 1; //need to convert the parseint to decimals?
-    } else if (this.topping === " Extra Cheese") {
-      this.price += 1;
-      console.log(this.price);
-    }
-     return this.price;
-     console.log(this.price);
-   }
+
+  // for (var i=0; i<=i.length;i++) { //looping through array to find which topping items were selected
+  //   if (this.topping === " Sausage" || this.topping === " Pepperoni") {
+  //     this.price += 2;
+  //   } else if (this.topping === " Mushrooms" || this.topping === " Green Peppers" || this.topping === " Olives") {
+  //     this.price += 1; //need to convert the parseint to decimals?
+  //   } else if (this.topping === " Extra Cheese") {
+  //     this.price += 1;
+  //     console.log(this.price);
+  //   }
+  return this.price;
+  console.log(this.price);
 }
 
-// var order = new PizzaOrder(0);
-// var size = order.pizzaCalculation(4);
-//
-// alert(order);
-
 function resetFields() {
-  $("input").val("");
-  $("option").val("");
+  $("#order-name").val("");
+  $("#size").val("");
+  $("#toppingSelection").val("");
 }
 
 // frontend logic
@@ -46,6 +44,7 @@ $(document).ready(function(){
     e.preventDefault();
 
     var orderName = $("#order-name").val();
+    var price = 0;
     var size = $("#size").val();
     var toppingChoiceArray = [];
 
@@ -55,23 +54,23 @@ $(document).ready(function(){
       $('.topping-output').text(toppingChoiceArray);
       console.log(toppingChoiceArray);
     });
-
     $("#order-output").append("<li><span class='pizzaOrderName'>" + orderName + ", click here for your order details" + "</span></li>"); //listing name for order details
 
-    var newPizzaOrder = new PizzaOrder(size, toppingChoiceArray); //instance that holds the values for each item
+    var newPizzaOrder = new PizzaOrder(price, size, toppingChoiceArray); //instance that holds the values for each item
 
     var pizzaPriceEstimator = newPizzaOrder.pizzaCalculation();  //create a variable to run a method to calculate the price
 
     console.log(newPizzaOrder);
     console.log(pizzaPriceEstimator);
     $(".pizzaOrderName").last().click(function(){
-      var orderInformation = `<img src='img/pizza.png'>
+      var orderInformation = `<h2>Order Details:</h2>
+                              <img src='img/pizza.png'>
                               <p class='lead outputDisplay'> ${orderName}, here are your order details:</p>
                               <p class='lead outputDisplay'> Pizza Size: ${size}</p>
                               <p class='lead outputDisplay'> Pizza Topping: ${toppingChoiceArray}</p>
                               <p class='lead outputDisplay'> Price: $ ${pizzaPriceEstimator}</p>
                               <button type="button" id="reset" class="btn btn-outline-warning btn-lg btn-block btn-style font-label">New Order</button>`;
-      $("#order-output").html(orderInformation);
+      $(".show-order").html(orderInformation);
     });
 
     // $(".pizzaOrderName").last().click(function(){
