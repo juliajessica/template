84ec353ffdd41c2a3a17a7f90907357f6f15259a
julia sheremet <juliajessicasher@gmail.com>
2018-04-06 09:00:49 -0700

    spec 1: program will return order name

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,18 @@
 
 
 
- 
+
 // frontend logic
+debugger;
+$(document).ready(function(){
+  debugger;
+  $("#pizza-form").submit(function(e){
+    e.preventDefault();
+
+
+  var orderName = $("#order-name").val();
+  debugger;
+
+  $("#order-output").append(orderName);
+  });
+});
